522351a29884ae16c1b33cdc1f844e5b4dbb9731
yoursharad78 <yoursharad78@gmail.com>
2019-04-24 08:54:58 +0530

    Hand cursor

---
+++ src/app/graph-service/service-constants.ts
@@ -0,0 +1,3 @@
+export enum ServiceConstants{
+    basePath = 'https://s3.eu-west-2.amazonaws.com/interview-question-data/metoffice/',
+}
--- src/app/graph-service/weather.service.ts
+++ src/app/graph-service/weather.service.ts
@@ -3,15 +3,16 @@ import { HttpClient } from '@angular/common/http';
 import { WeatherInput } from './weather-input.model';
 import { Observable} from 'rxjs';
 import { WeatherData } from './weather-data.model';
+import { ServiceConstants } from './service-constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  basePath: string;
+  //basePath: string;
   constructor(private httpClient: HttpClient) {
-    this.basePath ='https://s3.eu-west-2.amazonaws.com/interview-question-data/metoffice/';
+    //this.basePath ='https://s3.eu-west-2.amazonaws.com/interview-question-data/metoffice/';
    }
 
    getWeatherData(weatherInput: WeatherInput): Observable<WeatherData> {
@@ -25,6 +26,6 @@ export class WeatherService {
      let region= weatherInput.RegionName;
      let matric= weatherInput.Metric;
 
-     return this.basePath+ `${matric}-${region}.json`;
+     return ServiceConstants.basePath + `${matric}-${region}.json`;
    }
 }
